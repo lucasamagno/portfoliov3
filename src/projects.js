9df595423310c31517008283b9699fff5a3721aa
Lucas Magno <lucasamagno@gmail.com>
2023-11-16 08:13:28 -0500

    run new build

--- src/projects.js
+++ src/projects.js
@@ -15,4 +15,12 @@ export const projects = [
         tools: ["WordPress", "BlueHost"],
         url: "https://www.affinitystrings.com/"
     },
+    {
+        id: 3,
+        title: " Simple Valuation",
+        image: require("./assets/simplevaluation.png"), 
+        description: "Senior design project while obtaining my BS in Computer Science. The platform was designed to intake financial data and determine whether a stock was a good investment or not. (Not live anymore, checkout the github repo)",
+        tools: [".NET", "Azure"],
+        url: "https://github.com/dylanrichards/simplevaluation"
+    },
 ]
