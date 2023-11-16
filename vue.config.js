ed16d21f303b58133f9396ec5a9f2fadd7643bd7
Lucas Magno <lucasamagno@gmail.com>
2023-11-16 07:54:56 -0500

    last few changes

--- vue.config.js
+++ vue.config.js
@@ -2,7 +2,5 @@ const { defineConfig } = require('@vue/cli-service');
 
 module.exports = defineConfig({
   transpileDependencies: true,
-  publicPath: process.env.NODE_ENV === 'production'
-    ? '/portfoliov3/' // Replace 'repository-name' with your GitHub repo name
-    : '/'
+  publicPath: "/"
 });
